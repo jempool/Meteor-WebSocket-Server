8945bf5c4bd18f074194aaac0dbadc4ab42d2d26
Jem Suarez <jempoolsmz@gmail.com>
2023-12-08 07:02:25 -0500

    Adding websockets file

---
+++ server/src/websockets/websockets.js
@@ -0,0 +1,37 @@
+import { WebApp } from 'meteor/webapp';
+import { Server } from 'socket.io';
+
+import { WEBSOCKETS_CHAT_EVENT, WEBSOCKETS_TYPING_EVENT } from '../utils/constants.js';
+import messageService from '../services/message.service.js';
+
+
+export default function startWebSocketServer() {
+  const httpServer = WebApp.httpServer;
+
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`${new Date()} - New connection ${socket.id}`);
+
+    // Listening for chat event
+    socket.on(WEBSOCKETS_CHAT_EVENT, function (data) {
+      messageService.addMessage(data);
+      io.sockets.emit(WEBSOCKETS_CHAT_EVENT, data);
+    });
+
+    // Listening for typing event
+    socket.on(WEBSOCKETS_TYPING_EVENT, function (data) {
+      io.sockets.emit(WEBSOCKETS_TYPING_EVENT, data);
+      socket.broadcast.emit(WEBSOCKETS_TYPING_EVENT, data);
+    });
+
+    socket.on("error", (error) => {
+      console.error("Socket error:", error);
+    });
+  });
+}
